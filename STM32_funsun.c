c5829390ead39be633810ec777be5cd48e95b899
HieuDT2108 <79426353+HieuDT2108@users.noreply.github.com>
2024-09-06 17:05:17 +0700

    Update STM32_funsun.c
    Demio

--- STM32_funsun.c
+++ STM32_funsun.c
@@ -14,7 +14,7 @@
 
 void NMI_Handler(void);
 void HardFault_Handler(void);
-void MemManage_Handler(void);
+void MemManage_Handler(void); // Demo edit funsun
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void SVC_Handler(void);
